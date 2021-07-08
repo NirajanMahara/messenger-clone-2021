bda11972196e8102a629057e85814901e10a9d82
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-08 10:48:08 +0545

    Add `const [input, setInput] = useState('');` to the `<input />` || mapped empty string `value={input}` to the `input` && `setInput` is updating as we type(event) `onChange={(event) => setInput(event.target.value)}`

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  console.log(input);
+
   return (
     <div className='App'>
       <h1>React Firebase Facebook Messenger Clone 🚀</h1>
 
-      <input />
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
       <button>Send Message</button>
       {/* <input /> */}
       {/* <button /> */}
