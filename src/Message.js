c9535c231c6684568e92410cdfc7c43488b1632a
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-08 21:08:10 +0545

    `import { Card, CardContent, Typography } from '@material-ui/core';` & wrap Message inside those 🗃

--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
 import './Message.css';
 
 function Message(props) {
   return (
-    <div>
-      <h2>
-        {props.username}: {props.text}
-      </h2>
+    <div className='message'>
+      <Card>
+        <CardContent>
+          <Typography color='white' variant='h5' component='h2'>
+            {props.username}: {props.text}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
