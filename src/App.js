50af4e6d57a7d1fd9b66052808a61c8ec16b3830
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-08 11:00:15 +0545

    Add `sendMessage` function `onClick={sendMessage}` to `<button />` 🚀

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['Morning', 'Hello', 'Whats up']);
 
   console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    // all the logic to send the message goes here...
+
+    // Spread out the messages array and append to input
+    setMessages([...messages, input]);
+
+    // Input becomes empty string again
+    setInput('');
+  };
 
   return (
     <div className='App'>
       <h1>React Firebase Facebook Messenger Clone 🚀</h1>
 
       <input value={input} onChange={(event) => setInput(event.target.value)} />
-      <button>Send Message</button>
-      {/* <input /> */}
-      {/* <button /> */}
+      <button onClick={sendMessage}>Send Message</button>
 
       {/* <Messages themselves /> */}
     </div>
